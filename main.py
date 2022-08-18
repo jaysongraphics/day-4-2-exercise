f556d869212cfb0d192d0c3bef12a375e0a079f6
Edward Arias <79659253+jaysongraphics@users.noreply.github.com>
2022-08-18 01:43:37 +0000

    added choice(

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ random_choice = random.randint(0, name_length - 1)
 person_who_pays = names[random_choice]
 
 print(person_who_pays + " is going to buy the meal today!")
+
+# or simply use choice
+
+# person_who_will_pay = random.choice(names)
+
+# print(person_who_will_pay)
+
+# print(person_who_will_pay + " is going to buy the meal today!")
